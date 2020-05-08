cc53c05ab148015475b5aec8170b560cd9d87b3c
semperry <ryan.curtis88@gmail.com>
2020-05-08 09:55:08 -0600

    wildcard match to help with routing react routes

--- server/index.js
+++ server/index.js
@@ -63,7 +63,7 @@ app.use("/api", planetRoutes);
 app.use("/api", speciesRoutes);
 app.use("/api", transportRoutes);
 
-app.get("/*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname), "..", "build", "index.html");
 });
 
