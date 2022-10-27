648724bd28c46d2f33b16e7b83ade30c869b7bd9
Ryan Curtis <ryan.curtis88@gmail.com>
2022-10-27 17:27:28 -0600

    removes weapon malfunction message

--- src/components/swapiHeader.js
+++ src/components/swapiHeader.js
@@ -7,10 +7,7 @@ function SwapiHeader() {
 			<div className="app-header">
 				<h1>SWAPI</h1>
 				<p>The Star Wars API</p>
-				<h2 className="pulsing">
-					Slight weapons malfunction, everything is fine, here, now... How are
-					you?
-				</h2>
+				
 				<p>
 					<Link to="/about#what-happened">(...looking for swapi.co?)</Link>
 				</p>
