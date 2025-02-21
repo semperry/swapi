ea9ae1176002211871d1b77bbef8215e55e0ccf2
Ryan Curtis <ryan.curtis88@gmail.com>
2025-02-21 12:05:30 -0700

    runs local db

--- server/app/dbConfig.js
+++ server/app/dbConfig.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const MONGODB_URI =
 	process.env.NODE_ENV === "production"
 		? process.env.MONGODB_URI
-		: process.env.MONGODB_URI;
-// : "mongodb://127.0.0.1:27017/swapi";
+		: "mongodb://127.0.0.1:27017/swapi";
 
 module.exports = () => {
 	mongoose
