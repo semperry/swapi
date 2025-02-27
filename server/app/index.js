6967a19255eb0470f71a3c18bb8143cbd79cb9c1
Ryan Curtis <ryan.curtis88@gmail.com>
2025-02-26 18:06:49 -0700

    refactors server workflow

---
+++ server/app/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+
+require("dotenv").config({ path: path.join(__dirname, "../../.env") });
+
+const dbConfig = require("../config/dbConfig");
+const applyRoutes = require("./routes");
+const applyMiddleware = require("./middleware");
+const { applyConfig } = require("../config/config");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dbConfig();
+
+applyConfig(app);
+applyMiddleware(app);
+applyRoutes(app);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../../client/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "../../client/dist", "index.html"));
+	});
+}
+
+const startServer = () => {
+	app.listen(PORT, () => {
+		console.log(`Server running on port ${PORT}`);
+	});
+};
+
+module.exports = { startServer, app };
---
+++ server/app/routes.js
@@ -0,0 +1,26 @@
+const adClickRoutes = require("../routes/adClickRoutes");
+const countRoutes = require("../routes/countRoutes");
+const filmRoutes = require("../routes/filmRoutes");
+const peopleRoutes = require("../routes/peopleRoutes");
+const planetRoutes = require("../routes/planetRoutes");
+const rootRoutes = require("../routes/rootRoutes");
+const speciesRoutes = require("../routes/speciesRoutes");
+const starshipRoutes = require("../routes/starshipRoutes");
+const vehicleRoutes = require("../routes/vehicleRoutes");
+
+const applyRoutes = (app) => {
+	app.use("/", rootRoutes);
+	app.use("/count", countRoutes);
+	app.use("/track", adClickRoutes);
+
+	app.use("/api", [
+		filmRoutes,
+		peopleRoutes,
+		planetRoutes,
+		speciesRoutes,
+		starshipRoutes,
+		vehicleRoutes,
+	]);
+};
+
+module.exports = applyRoutes;
